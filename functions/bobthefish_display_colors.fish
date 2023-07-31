45cf8092edbd15135f3b42c6896c4e9eb1d0bb32
Matthias Fischer <m@tthias.space>
2023-07-31 03:16:10 +0200

    Update bobthefish_display_colors.fish

--- functions/bobthefish_display_colors.fish
+++ functions/bobthefish_display_colors.fish
@@ -3,8 +3,8 @@ function bobthefish_display_colors -a color_scheme -d 'Print example prompt colo
   set -l color_schemes default light \
     solarized solarized-light \
     base16 base16-light \
-    gruvbox zenburn \
-    dracula nord \
+    gruvbox gruvbox-light zenburn \
+    dracula dracula2 nord jellybeans \
     terminal terminal-dark-white \
     terminal-light terminal-light-black \
     terminal2 terminal2-dark-white \
